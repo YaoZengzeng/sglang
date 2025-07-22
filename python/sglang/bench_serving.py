82b10ee173d3192f617c70522f3f39c9c3bb6440
YaoZengzeng <yaozengzeng@huawei.com>
2025-07-22 08:20:02 +0000

    enable cache

--- python/sglang/bench_serving.py
+++ python/sglang/bench_serving.py
@@ -1146,10 +1146,10 @@ def sample_generated_shared_prefix_requests(
     cache_path = get_gen_prefix_cache_path(args, tokenizer)
 
     # Try to load from cache first
-    #if cache_path.exists():
-    #    print(f"\nLoading cached generated input data from {cache_path}")
-    #    with open(cache_path, "rb") as f:
-    #        return pickle.load(f)
+    if cache_path.exists():
+        print(f"\nLoading cached generated input data from {cache_path}")
+        with open(cache_path, "rb") as f:
+            return pickle.load(f)
 
     print("\nGenerating new input data...")
 
